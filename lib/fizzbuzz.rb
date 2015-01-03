05212abfdecb1af0624649d26ea39ed670fe5aee
Craig Horsborough <craighorsborough@gmail.com>
2015-01-03 16:32:38 +0000

    All Done, 10 tests passing

--- lib/fizzbuzz.rb
+++ lib/fizzbuzz.rb
@@ -4,4 +4,19 @@ class FizzBuzz
 		number % 3 == 0
 	end
 
+	def isDivisibleByFive(number)
+		number % 5 == 0
+	end
+
+	def isDivisibleByFifteen(number)
+		number % 15 == 0
+	end
+
+	def say(number)
+		return "FizzBuzz" if isDivisibleByFifteen(number)
+		return "Fizz" if isDivisibleByThree(number)
+		return "Buzz" if isDivisibleByFive(number)
+		return number
+	end
+
 end
--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -12,4 +12,36 @@ require 'fizzbuzz'
 			expect(fizzbuzz.isDivisibleByThree(1)).to eq false
 		end
 
+		it "Should know when a number is divisible by five" do
+			expect(fizzbuzz.isDivisibleByFive(5)).to eq true
+		end
+
+		it "Should know when a number isn't divisible by five" do
+			expect(fizzbuzz.isDivisibleByFive(1)).to eq false
+		end
+
+		it "Should know when a number is divisible by fifteen" do 
+			expect(fizzbuzz.isDivisibleByFifteen(15)).to eq true
+		end
+
+		it "Should know when a number isn't divisible by fifteen" do
+			expect(fizzbuzz.isDivisibleByFifteen(1)).to eq false
+		end
+
+		it "Should say 'Fizz' if the number is divisible by three" do 
+			expect(fizzbuzz.say(3)).to eq "Fizz"
+		end
+
+		it "Should say 'Buzz' if the number is divisible by five" do 
+			expect(fizzbuzz.say(5)).to eq "Buzz"
+		end
+
+		it "Should say 'FizzBuzz' if the number is divisible by fifteen" do 
+			expect(fizzbuzz.say(15)).to eq "FizzBuzz"
+		end
+
+		it "Should say the number if not a FizzBuzz number" do 
+			expect(fizzbuzz.say(1)).to eq 1
+		end
+
 	end
